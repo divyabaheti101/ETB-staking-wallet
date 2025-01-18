cf98e89259645f503dd530d196442e4bed7956b6
Divya <divyabaheti101@gmail.com>
2025-01-18 17:51:51 +0530

    wagmi setup index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+
+const {chains, providers } = configureChains(
+  [chain.mainnet,
+    chain.arbitrum,
+    chain.sepolia,
+    chain.polygon
+  ],
+  [
+    alchemyProvider({alchemyId: process.env.ALCHEMY_KEY}),
+    publicProvider()
+  ]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'StakingWalletDapp',
+  chains
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  providers
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient} >
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
 
